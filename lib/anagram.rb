ce2166b6d9fca2eb834e78563fb7c8a8b29f2b15
reddevilcero <reddevil_cero@hotmail.com>
2020-01-31 15:37:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    self.word = word
+  end
+
+  def match(array) 
+      anagrams = []
+      array.each do |word| 
+        check1 = word.size == self.word.size
+        check2 = (self.word.split('') - word.split('')).empty?
+        if check1 && check2
+          anagrams << word 
+        end
+      end
+      anagrams
+  end
+end
